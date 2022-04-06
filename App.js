40c71d2c43f242c22bf33a598f43c4dcd9631afb
senoralikz <alexjm825@gmail.com>
2022-04-06 18:50:57 -0400

    created header component and added uuid module

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "react-native-get-random-values";
+import Header from "./components/Header";
 
-export default function App() {
+const App = () => {
+  const [items, setItems] = useState([
+    { id: uuidv4(), text: "Milk" },
+    { id: uuidv4(), text: "Eggs" },
+    { id: uuidv4(), text: "Bread" },
+    { id: uuidv4(), text: "Juice" },
+  ]);
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      {/* <Text style={styles.text}>Welcome to the Journey</Text> */}
+      <Header title="Shopping List" />
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
+    // color: '#fff',
+    // backgroundColor: '#000',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  text: {
+    color: "#fff",
+    fontSize: 30,
   },
 });
+
+export default App;
